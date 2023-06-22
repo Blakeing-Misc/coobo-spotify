8090f43e3532bdc238a3f18d2e58f76ee8394896
Blake Ingenthron <blakeingenthron@gmail.com>
2023-06-22 17:21:40 -0500

    initial commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "misc.scdn.co",
+      "i.scdn.co",
+      "geo-media.beatsource.com",
+      "kzmkcdbfnxddbhwhtaiz.supabase.co",
+      "i1.sndcdn.com",
+      "media.pitchfork.com",
+      "seed-mix-image.spotifycdn.com",
+      "tbvswgcbrilqlaxdsxii.supabase.co",
+    ],
+  },
+};
+
+module.exports = nextConfig;
